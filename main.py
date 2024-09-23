cec04188449a9f7facd73510417e7c0b1efd9b9c
longeacc <clement.longeac@edu.esiee.fr>
2024-09-23 12:46:21 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 #### Fonction secondaire
-
-
-def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+"""Un palindrome est un mot ou une phrase qui 
+se lit indifféremment de droite à gauche et de 
+gauche à droite."""
+def ispalindrome(s)->bool:
+    """Cette fonction prend en argument une chaine de caractère et renvoie un bouléen"""
+    accents="éèêëàâäùûüôöïîç"
+    sans_accents="eeeeaaaouuooiic" # Correspondance exacte pour chaque accent
+    trans_table= str.maketrans(accents, sans_accents)
+    s=s.lower()
+    s = s.translate(trans_table)
+    s= ''.join(c for c in s if c.isalnum())
+    return s==s[::-1]
 
 #### Fonction principale
-
-
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """cette fonction est la fonction main"""
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
-
-
 if __name__ == "__main__":
     main()
